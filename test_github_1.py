77ca8492209d423169d2920faa79f1f3a4a1a825
baekgigwang-phoenix <creater9@naver.com>
2026-01-06 12:05:03 +0900

    update test_github_1.py
    add some code

--- test_github_1.py
+++ test_github_1.py
@@ -1,3 +1,10 @@
 # 이건 github와 github desktop 사용 테스트
 # 새로운 브랜치로 수정사항이 잘 적용되는지.
 # 이 파일은 새로운 파일
+
+import socket
+import cv2
+import numpy as np
+import time
+from pymycobot.mycobot280 import MyCobot280
+
